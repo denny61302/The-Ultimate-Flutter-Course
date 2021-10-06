da763502418cb64ce0ca22bb130b6e940f88d962
denny61302 <denny61302@gmail.com>
2021-10-06 14:57:01 +1030

    Module 1

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/features/movie_flow/movie_flow.dart';
+import 'package:my_app/theme/custom_theme.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
-
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
-
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+void main() {
+  runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movie Recommendation',
+      darkTheme: CustomTheme.darkTheme(context),
+      themeMode: ThemeMode.dark,
+      home: const MovieFlow(),
+    );
+  }
+}
+
+
+
+
